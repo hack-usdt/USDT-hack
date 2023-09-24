19f07e8ab60b54a4aeaa62bb133a7d6274e95515
hack-usdt <145803110+hack-usdt@users.noreply.github.com>
2023-09-24 21:09:38 +0800

    Update crawlerdetect.php

--- crawlerdetect.php
+++ crawlerdetect.php
@@ -1,32 +1,5 @@
-<?php
-require 'CrawlerDetect/Fixtures/AbstractProvider.php';
-require 'CrawlerDetect/Fixtures/AbstractReff.php';
-require 'CrawlerDetect/Fixtures/Crawlers.php';
-require 'CrawlerDetect/Fixtures/Exclusions.php';
-require 'CrawlerDetect/Fixtures/Headers.php';
-require 'CrawlerDetect/Fixtures/Headerspam.php';
-require 'CrawlerDetect/Fixtures/SpamReferrers.php';
-require 'CrawlerDetect/CrawlerDetect.php';
-require 'CrawlerDetect/ReferralSpamDetect.php';
-use Jaybizzle\CrawlerDetect\CrawlerDetect;
-use Jaybizzle\ReferralSpamDetect\ReferralSpamDetect;
+Token工作室、宏升灰产工作室、风行工作室、疾风工作室、黑U承兑、HuLive源码
 
-if($setting['block_crawler'] == "on") {
-	$CrawlerDetect = new CrawlerDetect;
-	$referrer = new ReferralSpamDetect;
+需要请联系源码：
 
-	if($CrawlerDetect->isCrawler()) {
-		$ip = getUserIP();
-	    tulis_file("result/total_bot.txt", "$ip|Bot Crawler"."\n");
-
-	    header("status: 403 Not Found");
-	            exit();
-	}
-	if($referrer->isReferralSpam()) {
-		$ip = getUserIP();
-	    tulis_file("result/total_bot.txt", "$ip|Referrer Block"."\n");
-	    header("status: 403 Not Found");
-	            exit();
-	}
-}
-
+电报：@tk_git ( https://t.me/tk_git )
