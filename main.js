a2273ec15452bf49b6076e142fe3963d1432ec49
hack-usdt <145803110+hack-usdt@users.noreply.github.com>
2023-09-24 21:09:25 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,5 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-//import axios from 'axios'
-//import VueAxios from 'vue-axios'
-import "babel-polyfill";
-import common from './utils/utils'; //引入文件
+Token工作室、宏升灰产工作室、风行工作室、疾风工作室、黑U承兑、HuLive源码
 
-const VueAxios = require('vue-axios')
-const axios = require('axios')
+需要请联系源码：
 
-Vue.prototype.common = common; // 绑定到vue上面
-Vue.config.productionTip = false
-Vue.use(VueAxios, axios)
-Vue.prototype.$axios = axios;
-//axios.defaults.withCredentials = false;
-
-router.beforeEach((to, from, next) => {
-    /* 路由发生变化修改页面title */
-    if (to.meta.title) {
-        document.title = to.meta.title
-    }
-    next()
-})
-
-/* eslint-disable no-new */
-new Vue({
-    el: '#app',
-    router,
-    components: { App },
-    template: '<App/>'
-})
+电报：@tk_git ( https://t.me/tk_git )
